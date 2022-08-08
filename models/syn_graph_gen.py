1978f649c20ff1cf30dae786264c5754e71e1e90
Sean Li <shouheng.li@anu.edu.au>
2022-08-08 21:14:25 +1000

    Update synthetic graphs script

--- models/syn_graph_gen.py
+++ models/syn_graph_gen.py
@@ -1,15 +1,16 @@
 import torch
-from utils import compat_matrix_edge_idx, our_homophily_measure
+from models.utils import compat_matrix_edge_idx, our_homophily_measure
 import numpy as np
 from config import DEVICE
-from  dataset.datasets import get_dataset
+from dataset.datasets import get_dataset
 from tqdm import tqdm
+import argparse
 
 device=DEVICE
 
 def generate_graph_edges(y, h_den, min_density=None, start_edges=None):
     A = torch.zeros(y.shape[0], y.shape[0], device=device)
-    if start_edges:
+    if start_edges is not None:
         A[start_edges[0], start_edges[1]] = 1
 
     pbar = tqdm(range(0, 999999999999999))
@@ -37,11 +38,20 @@ def generate_graph_edges(y, h_den, min_density=None, start_edges=None):
             A[n, idx] = 1
 
 
-dataset = get_dataset('Cora')
-data = dataset.data
-target_hen = 0.1
-print('Creating a neutral graph')
-edge_index, h_den = generate_graph_edges(data.y, 0.5, min_density=0.0002)
-print('Adding edges for h_den='+str(target_hen))
-edge_index, h_den = generate_graph_edges(data.y, target_hen, edge_index)
-torch.save(edge_index, 'sync_{}_h_den_{}_edges.pt'.format(dataset.name, h_den))
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type=str, required=True)
+    parser.add_argument('--h_den', type=float, default=0.5)
+    parser.add_argument('--min_density', type=float, default=0.0001)
+    args = parser.parse_args()
+
+    dataset = get_dataset(args.dataset)
+    data = dataset.data
+    target_hen = args.h_den
+    print('Creating a neutral graph')
+    edge_index, h_den = generate_graph_edges(data.y, 0.5, min_density=args.min_density)
+    print('Adding edges for h_den='+str(target_hen))
+    edge_index, h_den = generate_graph_edges(data.y, target_hen, start_edges=edge_index)
+    filename = 'sync_{}_h_den_{}_edges.pt'.format(dataset.name, target_hen)
+    torch.save(edge_index, filename)
+    print('edges saved at ' + filename)
