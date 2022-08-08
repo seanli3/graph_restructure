331d8820e6444174aa36731bb75518ac498d857a
Sean Li <shouheng.li@anu.edu.au>
2022-08-08 20:42:38 +1000

    Update synthetic graphs script

--- models/syn_graph_gen.py
+++ models/syn_graph_gen.py
@@ -2,52 +2,46 @@ import torch
 from utils import compat_matrix_edge_idx, our_homophily_measure
 import numpy as np
 from config import DEVICE
+from  dataset.datasets import get_dataset
+from tqdm import tqdm
 
 device=DEVICE
 
-def generate_graph_edges(y, h_den):
-    num_class = y.max().item() + 1
-    h_hat = 2*h_den - 1
-    counts = y.unique(return_counts=True)[1].float()
-    counts = counts.view(-1, 1).mm(counts.view(1, -1))
+def generate_graph_edges(y, h_den, min_density=None, start_edges=None):
+    A = torch.zeros(y.shape[0], y.shape[0], device=device)
+    if start_edges:
+        A[start_edges[0], start_edges[1]] = 1
 
-    if h_hat >= 0:
-        h_intra = np.random.rand() * (1-h_hat) + h_hat
-        h_inter = h_intra - h_hat
-    else:
-        h_inter = np.random.rand() * (1+h_hat) - h_hat
-        h_intra = h_inter + h_hat
-
-    intra_pro = h_intra * counts.diag().sum()
-    inter_pro = h_inter * ((counts.sum() - counts.diag().sum())/(num_class-1))
-    scaled_intra_pro = intra_pro / (inter_pro + intra_pro)
-    scaled_inter_pro = inter_pro / (inter_pro + intra_pro)
-
-    edges = []
-    cur_homo = None
-    while cur_homo is None or np.abs(cur_homo - h_den) > 0.0001:
-        draw = np.random.uniform()
-        if draw < scaled_intra_pro:
-            intra = True
-        else:
-            intra = False
+    pbar = tqdm(range(0, 999999999999999))
+    for _ in pbar:
         for idx, i in enumerate(y):
-            if intra is True:
-                n = np.random.choice((y == i).nonzero().view(-1).cpu())
+            edge_index = A.nonzero().T
+            cur_homo = our_homophily_measure(edge_index, y).item()
+            pbar.set_description('h_den: ' + str(cur_homo))
+            if np.abs(cur_homo - h_den) < 0.0001:
+                if min_density is not None:
+                    if A.count_nonzero() / A.numel() >= min_density:
+                        return edge_index, cur_homo
+                else:
+                    return edge_index, cur_homo
+            if cur_homo <= h_den:
+                intra = True
             else:
-                n = np.random.choice((y != i).nonzero().view(-1).cpu())
-            e = (idx, n)
-            if e not in edges:
-                edges.append(e)
-                if e[1] != e[0]:
-                    edges.append((e[1], e[0]))
-        edge_index = torch.tensor(edges, device=device).T
-        cur_homo = our_homophily_measure(edge_index, y).item()
-        H = compat_matrix_edge_idx(edge_index, y)
-        if torch.allclose(H, counts):
-            break
-        print(cur_homo)
-    print(edges)
+                intra = False
+
+            if intra is True:
+                n = np.random.choice((y == i).logical_and(A[idx] == 0).nonzero().view(-1).cpu())
+            else:
+                n = np.random.choice((y != i).logical_and(A[idx] == 0).nonzero().view(-1).cpu())
+            A[idx, n] = 1
+            A[n, idx] = 1
 
 
-generate_graph_edges(torch.tensor([0,1,0,1,2,2,0,0,1,1,2], device=device), 0.7083)
+dataset = get_dataset('Cora')
+data = dataset.data
+target_hen = 0.1
+print('Creating a neutral graph')
+edge_index, h_den = generate_graph_edges(data.y, 0.5, min_density=0.0002)
+print('Adding edges for h_den='+str(target_hen))
+edge_index, h_den = generate_graph_edges(data.y, target_hen, edge_index)
+torch.save(edge_index, 'sync_{}_h_den_{}_edges.pt'.format(dataset.name, h_den))
